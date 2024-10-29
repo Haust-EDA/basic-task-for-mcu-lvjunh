034e2af41c2bf27eac3e278b9487e3a8e9cef398
lvjunh <1322069095@qq.com>
2024-10-29 18:51:59 +0800

    first commit

--- task.c
+++ task.c
@@ -21,14 +21,14 @@ int main(void)
     """
     在下方编写你的代码
     """
-    if(x==1000)
+    while (1)
     {
-        for(i=0;i<19601;i++)//延时1s
+        for (i = 0; i < 8; i++)  // 从左到右依次点亮
         {
-            for(j=5;j>0;j--);
+            P0 = led_code[i];  // 输出控制码点亮相应的灯
+            delay_ms(500);  // 延时 500ms
         }
     }
-    else while(x--)for(j=115;j>0;j--);
 
  
 
